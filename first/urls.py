c5854c3290b420fe6ffa14c6afb9229f3efafa2a
EmmanuelAttah1 <attahzuzu@gmail.com>
2025-05-14 10:57:50 +0100

    create detail view and url path

--- first/urls.py
+++ first/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home',views.home,name="home"),
+    path('detail',views.details, name="detail")
 ]
--- first/views.py
+++ first/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Home Page</h1>")
+
+def details(request):
+    return HttpResponse("Details page")
